07c1d1a5daf40fba025a7f4d8f48a313957ec3aa
kimdonghyeon3 <sh2115521@naver.com>
2022-09-14 00:53:12 +0900

    feat : test 구현

--- src/main/java/com/ll/exam/app10/app/member/MemberController.java
+++ src/main/java/com/ll/exam/app10/app/member/MemberController.java
@@ -1,19 +1,25 @@
 package com.ll.exam.app10.app.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/member/join")
     public String join (){
@@ -21,30 +27,30 @@ public class MemberController {
     }
 
     @PostMapping("/member/join")
-    public String join(String username, String password, String email, MultipartFile profileImg, HttpSession session) {
+    public String join(String username, String password, String email, MultipartFile profileImg, HttpServletRequest request) {
         Member oldMember = memberService.getMemberByUsername(username);
+
+        String passwordClearText = password;
+        password = passwordEncoder.encode(password);
 
         if (oldMember != null) {
             return "redirect:/?errorMsg=Already done.";
         }
 
-        Member member = memberService.join(username, "{noop}" + password, email, profileImg);
-
-        session.setAttribute("loginedMemberId", member.getId());
+        Member member = memberService.join(username, password, email, profileImg);
+        try {
+            request.login(username, passwordClearText);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
 
         return "redirect:/member/profile";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/member/profile")
-    public String showProfile(HttpSession session, Model model) {
-        Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
-        boolean isLogined = loginedMemberId != null;
-
-        if (isLogined == false) {
-            return "redirect:/?errorMsg=Need to login!";
-        }
-
-        Member loginedMember = memberService.getMemberById(loginedMemberId);
+    public String showProfile(Principal principal, Model model) {
+        Member loginedMember = memberService.getMemberByUsername(principal.getName());
 
         model.addAttribute("loginedMember", loginedMember);
 
--- src/main/java/com/ll/exam/app10/app/member/MemberService.java
+++ src/main/java/com/ll/exam/app10/app/member/MemberService.java
@@ -3,16 +3,24 @@ package com.ll.exam.app10.app.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
     @Value("${custom.genFileDirPath}")
     private String genFileDirPath;
 
@@ -48,5 +56,39 @@ public class MemberService {
 
     public Member getMemberById(Long id) {
         return memberRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).get();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("member"));
+
+        return new User(member.getUsername(), member.getPassword(), authorities);
+    }
+
+    public Member join(String username, String password, String email) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
+
+        memberRepository.save(member);
+
+        return member;
+    }
+
+    public long count() {
+        return memberRepository.count();
+    }
+
+    public void removeProfileImg(Member member) {
+        member.removeProfileImgOnStorage(); //실제 파일 삭제
+
+        member.setProfileImg(null);
+        memberRepository.save(member);
+
     }
 }
