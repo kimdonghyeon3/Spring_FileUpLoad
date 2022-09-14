a0c6850b6138f16a107d359051879a6fc920accc
kimdonghyeon3 <sh2115521@naver.com>
2022-09-14 14:16:45 +0900

    feat : about 페이지 추가

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -19,9 +19,8 @@ import java.util.UUID;
 public class HomeController {
 
     private final MemberService memberService;
-    @RequestMapping("/")
-    public String main(Principal principal, Model model) {
-
+    @GetMapping("/")
+    public String showMain(Principal principal, Model model) {
         Member loginedMember = null;
         String loginedMemberProfileImgUrl = null;
 
@@ -39,9 +38,23 @@ public class HomeController {
         return "home/main";
     }
 
-    @RequestMapping("/test/upload")
-    public String upload() {
-        return "home/test/upload";
+    @GetMapping("/about")
+    public String showAbout(Principal principal, Model model) {
+        Member loginedMember = null;
+        String loginedMemberProfileImgUrl = null;
+
+        if (principal != null && principal.getName() != null) {
+            loginedMember = memberService.getMemberByUsername(principal.getName());
+        }
+
+        if (loginedMember != null) {
+            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
+        }
+
+        model.addAttribute("loginedMember", loginedMember);
+        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
+
+        return "home/about";
     }
 
 }
